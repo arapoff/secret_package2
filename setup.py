7fa51c765f577947fca1753661ec5d760ca0a3d5
Sultan Arapov <31477622+arapoff@users.noreply.github.com>
2022-08-11 15:53:37 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name="secret_package",
     install_requires=[
-        "importlib-metadata; python_version < '3.8'",
-        "subprocess.run"
+        "importlib-metadata; python_version < '3.8'"
     ],
 )
