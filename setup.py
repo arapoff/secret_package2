d61dbc90c172d8836f1646a201d907969d4aa540
Sultan Arapov <31477622+arapoff@users.noreply.github.com>
2022-08-20 14:35:12 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 import subprocess
 
-setup(
-    name="secret_package2",
-    packages=['secret_package2'],
-    zip_safe=False
-)
+setup(name='secret_package2',
+      version='1.0.0',
+      description='package desc',
+      url='package url',
+      author='package Author',
+      author_email='package Author email',
+      license='MIT',
+      packages=['secret_package2'],
+      zip_safe=False)
