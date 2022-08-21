5e57b007c61b039653bca9dd3275feace946386e
Sultan Arapov <31477622+arapoff@users.noreply.github.com>
2022-08-21 11:14:59 +0600

    Update and rename call_subprocess.py to subprocesses.py

--- secret_package2/subprocesses.py
+++ secret_package2/subprocesses.py
@@ -1,7 +1,12 @@
 import subprocess
 
-def call_bwa():
-  subprocess.call(['bwa', 'index']) 
-  subprocess.call(['samtools', 'faidx'])
+def some_function():
+	## index them now
+	subprocess.call(['samtools', 'faidx', output_fasta_filename])  
+	subprocess.call(['bwa', 'index', output_fasta_filename])  	
+	## also need samtools dict
+	subprocess.call(['samtools', 'dict', output_fasta_filename, '-o', output_fasta_dict_filename])
+  
   gatk_rg = subprocess.call(['gatk', 'AddOrReplaceReadGroups', '--INPUT', output_bam_sorted, '--OUTPUT', output_RG_bam_sorted, '--RGLB', 'lib1', '--RGPL', 'illumina', '--RGPU', 'unit1', '--RGSM', '"CONTIG_ID"'])
+  
   subprocess.call(['whatshap', 'polyphase', '--ignore-read-groups', '--ploidy', str(row['estimated_ploidy']), '-o', output_phased_vcf, '--reference={}'.format(row['fasta_files']), nucmer_pair_dfs['HaplotypeCaller_VCFs'], output_bam_sorted])
