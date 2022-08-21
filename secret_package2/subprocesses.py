c124d2b4dd30f81af4039a1ac29a30171a5b885d
Sultan Arapov <31477622+arapoff@users.noreply.github.com>
2022-08-21 12:27:59 +0600

    Update subprocesses.py

--- secret_package2/subprocesses.py
+++ secret_package2/subprocesses.py
@@ -9,10 +9,10 @@ def generate_bwa_fasta(output_fasta_filename):
 def generate_sam_fasta_dict(output_fasta_filename, output_fasta_dict_filename):  
   subprocess.call(['samtools', 'dict', output_fasta_filename, '-o', output_fasta_dict_filename])
   
-def generate_gatk_vcf(output_bam_sorted, output_RG_bam_sorted):
+def generate_bam(output_bam_sorted, output_RG_bam_sorted):
   gatk_rg = subprocess.call(['gatk', 'AddOrReplaceReadGroups', '--INPUT', output_bam_sorted, '--OUTPUT', output_RG_bam_sorted, '--RGLB', 'lib1', '--RGPL', 'illumina', '--RGPU', 'unit1', '--RGSM', '"CONTIG_ID"'])
-
-def generate_whatshap_vcf(row, output_phased_vcf, nucmer_pair_dfs, output_bam_sorted):
+  
+def generate_vcf(row, output_phased_vcf, nucmer_pair_dfs, output_bam_sorted):
   subprocess.call(['whatshap', 'polyphase', '--ignore-read-groups', '--ploidy', str(row['estimated_ploidy']), '-o', output_phased_vcf, '--reference={}'.format(row['fasta_files']), nucmer_pair_dfs['HaplotypeCaller_VCFs'], output_bam_sorted])
 
 
